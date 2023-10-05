5c20a8dc3e874f71a16cc6a73b9112015b124a1c
amitaiturkel <amitai.turkel@gmail.com>
2023-10-05 09:54:48 +0300

    try

--- server-fastapi-comm-to-nats-docker/poetry-demo/poetry_demo/fastapi_server.py
+++ server-fastapi-comm-to-nats-docker/poetry-demo/poetry_demo/fastapi_server.py
@@ -55,5 +55,5 @@ async def connect_nats(num: str = Header(None),operator: str = Header(None),
         print("An error occurred while communicating with the calculator service:", str(e))
 
 if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
